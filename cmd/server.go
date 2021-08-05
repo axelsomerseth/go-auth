8218a9c321e360ba9a1fd91b8e8931882a83655f
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2021-08-04 21:14:29 -0600

    ataching default middlewares

--- cmd/server.go
+++ cmd/server.go
@@ -10,7 +10,9 @@ func main() {
 	router := gin.New()
 	router.RedirectTrailingSlash = true
 
-	// Attach middlewares to router
+	// Attach middlewares to router.
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
 
 	router.GET("/status", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
